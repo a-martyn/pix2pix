5c2840ec5a2e0a02cd9a91aa5da639da1ec50c49
a-martyn <a-martyn@users.noreply.github.com>
2019-02-27 16:55:04 +0000

    generator update

--- README.md
+++ README.md
@@ -1,6 +1,57 @@
+
+
+### Implementation Notes
+
+
+What is a ReLU?
+
+- a Rectified Linear Unit is an activation function that sets all negative values to zero. Positive values are treated linearly. Similar to an ideal rectifier in electronics. 
+
+What is a Leaky ReLU?
+
+- Leaky ReLUs allow a small positive gradient for negative values. 
+- e.g. if x>0: f(x) = x, else: f(x) = 0.01x
+
+Should activations be passed to skip connection before ReLU is applied?
+- The pix2pix code applies ReLU at input to each block, rather than at ouput.
+- The pix2pix code is difficult to interpret here, not sure if skips should be pro-or post activation? The difference would be in whether LeakyReLU or ReLU is applied to skip activations. Not sure. Will try later
+
+Should innermost layers of U-net have a skip connection?
+- see my sketch in appendix to show that no, it shouldn't 
+- pix2pix code excludes skip connections from innermost outermost layers
+
+Where should skip connections be concatenated?
+
+Is dropout in pix2pix repo as described in paper?
+
+What are model adjustments for color transfer?
+
+Should u-net have odd number of layers?
+- unet in orginal paper has odd number of layers, but pix2pix has two unconnected layers at bottom of U
+
+How should upsampling be performed in the U-net generator?
+- [30] suggests "fractional-strided convolutions", which I assume means for example a stride of 0.5. It is suggested that downsampling and upsampling with strided convolutions is superior to spatial pooling functions because they allow the network to learn its own spatial downsampling and upsampling.
+- [34] unet papers suggests 'Every step in the expansive path consists of an upsampling of the feature map followed by a 2x2 convolution (“up-convolution”) that halves the number of feature channels, a concatenation with the correspondingly cropped feature map from the contracting path, and two 3x3 convolutions, each fol- lowed by a ReLU.", must assume stride=1, else no upsampling would be achieved
+- The pix2pix implementation uses the `ConvTranspose2d` pytorch layer for upsampling, the docs for which note this function is otherwise known as fractional-strided convolution as mentioned by [30], so that ties together.
+- Keras has a `Conv2DTranspose` layer which seems to be equivalent so I'll use that.
+
+What is softmax function?
+
+What is entropy?
+
+What is cross-entropy loss?
+
+What is the difference between **fractionally-strided convolution**  and **dilated convolution**? 
+
+Which layers should include bias?
+
+Why are only 2 output channels used in colorization?
+
+
 ### References
 
+- [30] A. Radford, L. Metz, and S. Chintala. Unsupervised representation learning with deep convolutional generative adversarial networks. arXiv preprint arXiv:1511.06434, 2015.
+- [34] O. Ronneberger, P. Fischer, and T. Brox. U-net: Convolutional networks for biomedical image segmentation. In MIC- CAI, pages 234–241. Springer, 2015. 2, 3, 4
 - https://arxiv.org/pdf/1611.07004.pdf
 - https://richzhang.github.io/ideepcolor/
 - http://richzhang.github.io/colorization/
-
--- model/generator.py
+++ model/generator.py
@@ -1,6 +1,9 @@
 import numpy as np
 from tensorflow import keras
-from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, UpSampling2D, Dropout, ReLU
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, BatchNormalization
+from tensorflow.keras.layers import UpSampling2D, Dropout, LeakyReLU, ReLU
+from tensorflow.keras.layers import Concatenate, Activation
+from tensorflow.keras.models import Model
 
 
 model = Sequential()
@@ -17,115 +20,168 @@ def unet(input_shape, output_channels):
     # SETTINGS
     
     # Convolutional layer
-    strides = 2
-    kernel_size = (4, 4) 
-    padding = 'same' # Todo: check padding, should be 1
-    # The data_loader returns inputs with shape (batch, height, width, channels)
-    df = "channels_last"
-    
-    # Upsampling
-    ups_size = 2
+    conv_kwargs = {
+        'kernel_size': (4, 4),
+        'strides': 2,
+        'padding': 'same',
+        'kernel_initializer': 'he_normal',
+        'data_format': 'channels_last'  # (batch, height, width, channels)
+    }
+
+    # strides = 2
+    # ks = (4, 4) 
+    # pad = 'same' # Todo: check padding, should be 1
+    # df = "channels_last"
     
     # Batch Normalisation
     # TODO: Instance Normalisation: batchnorm use test stats at test time
     # TODO: pytorch implementation has learnable params, how to implement in keras?
-    bn_axis = -1    # because data_loader returns channels last
-    bn_mmtm = 0.9   # equivalent to pytorch defaults used by author (0.1 in pytorch -> 0.9 in keras/tf)
-    bn_eps  = 1e-5  # match pytorch defaults
-    
+    # bn_axis = -1    # because data_loader returns channels last
+    # bn_mmtm = 0.9   # equivalent to pytorch defaults used by author (0.1 in pytorch -> 0.9 in keras/tf)
+    # bn_eps  = 1e-5  # match pytorch defaults
+    bn_kwargs = {
+        'axis': -1,       # because data_loader returns channels last
+        'momentum': 0.9,  # equivalent to pytorch defaults used by author (0.1 in pytorch -> 0.9 in keras/tf)
+        'epsilon': 1e-5   # match pytorch defaults
+    }
+
     # ReLU
     slope = 0.2
-    
-    # Skip connections
-    merge_mode = 'concat'
-    
     # Dropout
     dropout = 0.5
-    
+
     # ----------------------------------------------------------------
     # ENCODER
-    
+
     # - Let Ck denote a Convolution-BatchNorm-ReLU layerwith k filter
     # - Convolutions downsample by a factor of 2
     # - All ReLUs are leaky
     # Architecture is then:
     # C64-C128-C256-C512-C512-C512-C512-C512
-    
+
     # TODO: check padding is correct
-    # TODO: check downsampling/upsampling behaviour is as expected
-    
+
+    # input
+    # (256 x 256 x input_channels)
+
     # layer 1 - C64
     # Batch-Norm is not applied to the first C64 layer
-    x1 = Conv2D(64, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df input_shape=input_shape)
-    x1 = LeakyReLU(alpha=slope)(x1)
-    
+    e1 = Conv2D(64, input_shape=input_shape, **conv_kwargs)
+    # (128 x 128 x 64)
+
     # layer 2 - C128
-    x2 = Conv2D(128, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x1)
-    x2 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x2)
-    x2 = LeakyReLU(alpha=slope)(x2)
-    
+    e2 = LeakyReLU(alpha=slope)(e1)
+    e2 = Conv2D(128, **conv_kwargs)(e2)
+    e2 = BatchNormalization(**bn_kwargs)(e2)
+    # (64 x 64 x 128)
+
     # layer 3 - C256
-    x3 = Conv2D(256, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x2)
-    x3 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x3)
-    x3 = LeakyReLU(alpha=slope)(x3)
-    
+    e3 = LeakyReLU(alpha=slope)(e2)
+    e3 = Conv2D(256, **conv_kwargs)(e3)
+    e3 = BatchNormalization(**bn_kwargs)(e3)
+    # (32 x 32 x 256)
+
     # layer 4 - C512
-    x4 = Conv2D(512, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x3)
-    x4 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x4)
-    x4 = LeakyReLU(alpha=slope)(x4)
-    
+    e4 = LeakyReLU(alpha=slope)(e3)
+    e4 = Conv2D(512, **conv_kwargs)(e4)
+    e4 = BatchNormalization(**bn_kwargs)(e4)
+    # (16 x 16 x 512)
+
     # layer 5 - C512
-    x5 = Conv2D(512, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x4)
-    x5 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x5)
-    x5 = LeakyReLU(alpha=slope)(x5)
-    
+    e5 = LeakyReLU(alpha=slope)(e4)
+    e5 = Conv2D(512, **conv_kwargs)(e5)
+    e5 = BatchNormalization(**bn_kwargs)(e5)
+    # (8 x 8 x 512)
+
     # layer 6 - C512
-    x6 = Conv2D(512, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x5)
-    x6 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x6)
-    x6 = LeakyReLU(alpha=slope)(x6)
-    
+    e6 = LeakyReLU(alpha=slope)(e5)
+    e6 = Conv2D(512, **conv_kwargs)(e6)
+    e6 = BatchNormalization(**bn_kwargs)(e6)
+    # (4 x 4 x 512)
+
     # layer 7 - C512
-    x7 = Conv2D(512, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x6)
-    x7 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x7)
-    x7 = LeakyReLU(alpha=slope)(x7)
-    
-    # layer 8 - C512    
-    x8 = Conv2D(512, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x7)
-    x8 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x8)
-    x8 = LeakyReLU(alpha=slope)(x8)   
-    
+    e7 = LeakyReLU(alpha=slope)(e6)
+    e7 = Conv2D(512, **conv_kwargs)(e7)
+    e7 = BatchNormalization(**bn_kwargs)(e7)
+    # (2 x 2 x 512)
+
+    # layer 8 - C512
+    e8 = LeakyReLU(alpha=slope)(e7)
+    e8 = Conv2D(512, **conv_kwargs)(e8)
+    # e8 = BatchNormalization(**bn_kwargs)(e8)  # not implemented by authors
+    # (1 x 1 x 512)
+
     # ----------------------------------------------------------------
     # DECODER
-    
+
     # - Ck denotes a Convolution-BatchNorm-ReLU layerwith k filters:
-    # - CDk denotes a a Convolution-BatchNorm-Dropout-ReLU layer 
+    # - CDk denotes a a Convolution-BatchNorm-Dropout-ReLU layer
     #   with a dropout rate of 50%
     # - Convolutions upsample by a factor of 2,
     # - All ReLUs are not leaky
     # Architecture is then:
     # CD512-CD1024-CD1024-C1024-C1024-C512-C256-C128
-    
+
     # layer 9 - CD512
-    x9 = UpSampling2D(size=ups_size, data_format=df)(x8) 
-    x9 = Conv2D(512, kernel_size=kernel_size, strides=strides, padding=padding, data_format=df)(x9)
-    x9 = BatchNormalization(axis=bn_axis, momentum=bn_mmtm, epsilon=bn_eps)(x9)
-    x9 = Dropout(p=dropout)(x9)
-    x9 = ReLU()(x9)
-    
-    # TODO: Where should skip connections be concatenated?
-    # Should updampling layers have stride 2? pix2pix implementation suggests yes
-    
+    d1 = LeakyReLU(alpha=slope)(e8)  # Note: authors use ReLU
+    d1 = Conv2DTranspose(512, **conv_kwargs)(d1)
+    d1 = BatchNormalization(**bn_kwargs)(d1)
+    d1 = Dropout(p=dropout)(d1)  # Note: pytorch pix2pix doesn't implement
+    # (2 x 2 x 512)
+
     # layer 10 - CD1024
+    d2 = Concatenate([d1, e7], axis=-1)
+    d2 = ReLU()(d2)
+    d2 = Conv2DTranspose(512, **conv_kwargs)(d2)
+    d2 = BatchNormalization(**bn_kwargs)(d2)
+    d2 = Dropout(p=dropout)(d2)
+    # (4 x 4 x 512)
+
     # layer 11 - CD1024
+    d3 = Concatenate([d2, e6], axis=-1)
+    d3 = ReLU()(d3)
+    d3 = Conv2DTranspose(512, **conv_kwargs)(d3)
+    d3 = BatchNormalization(**bn_kwargs)(d3)
+    d3 = Dropout(p=dropout)(d3)
+    # (8 x 8 x 512)
+
     # layer 12 - C1024
+    d4 = Concatenate([d3, e5], axis=-1)
+    d4 = ReLU()(d4)
+    d4 = Conv2DTranspose(512, **conv_kwargs)(d4)
+    d4 = BatchNormalization(**bn_kwargs)(d4)
+    # (16 x 16 x 512)
+
     # layer 13 - C1024
+    d5 = Concatenate([d4, e4], axis=-1)
+    d5 = ReLU()(d5)
+    d5 = Conv2DTranspose(256, **conv_kwargs)(d5)
+    d5 = BatchNormalization(**bn_kwargs)(d5)
+    # (32 x 32 x 256)
+
     # layer 14 - C512
+    d6 = Concatenate([d5, e3], axis=-1)
+    d6 = ReLU()(d6)
+    d6 = Conv2DTranspose(128, **conv_kwargs)(d6)
+    d6 = BatchNormalization(**bn_kwargs)(d6)
+    # (64 x 64 x 128)
+
     # layer 15 - C256
-    # layer 16 - C128
-    
-    
-    
-    
-    # After the last layer in the decoder, a convolution is applied 
+    d7 = Concatenate([d6, e2], axis=-1)
+    d7 = ReLU()(d7)
+    d7 = Conv2DTranspose(64, **conv_kwargs)(d7)
+    d7 = BatchNormalization(**bn_kwargs)(d7)
+    # (128 x 128 x 64)
+
+    # In the last layer in the decoder, a convolution is applied 
     # to map to the number of output channels (3 in general, except 
     # in colorization, where it is 2), followed by a Tanh function. 
+    # layer 16 - C128
+    d8 = Concatenate([d7, e1], axis=-1)
+    d8 = ReLU()(d8)  
+    d8 = Conv2DTranspose(output_channels, **conv_kwargs)(d8)
+    d8 = Activation('tanh')(d8)
+    # (256 x 256 x output_channels)
+
+    return Model(input=[e1], output=[d8], name='unet')
+    
